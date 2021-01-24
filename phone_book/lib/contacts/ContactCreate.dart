aaa850b94134483b5463bf2d26c654c49a62353c
remzigunes21 <remzi.gunes1283@gmail.com>
2021-01-24 11:01:32 +0300

    db helper

--- phone_book/lib/contacts/ContactCreate.dart
+++ phone_book/lib/contacts/ContactCreate.dart
@@ -1,15 +1,149 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:phone_book/database/db_helper.dart';
 import 'package:phone_book/model/Contact.dart';
 
 class ContactCreate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Contact.contacts.add(Contact(name: "Remzi", phoneNumber: "04455667676"));
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("Add New Contacts"),
       ),
+      body: SingleChildScrollView(child: ContactForm()),
     );
+  }
+}
+
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  String name;
+  String phoneNumber;
+
+  File _file;
+  DbHelper _dbHelper;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    _dbHelper = DbHelper();
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Container(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Image.asset(
+                _file == null ? "assets/img/person.jpg" : _file.path,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 250,
+              ),
+              Positioned(
+                  bottom: 8,
+                  right: 8,
+                  child: IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.camera_alt),
+                    onPressed: getFile,
+                  )),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "name is required";
+                        }
+                      },
+                      onSaved: (newValue) {
+                        name = newValue;
+                      },
+                      decoration: InputDecoration(hintText: "Contact Name"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(hintText: "Phone Number"),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "phone is required";
+                        }
+                      },
+                      onSaved: (newValue) {
+                        phoneNumber = newValue;
+                      },
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+
+                        var contact = Contact(
+                            name: name,
+                            phoneNumber: phoneNumber,
+                            avatar: _file == null ? "" : _file.path);
+
+                        await _dbHelper.insertContact(contact);
+
+                        var snackBar = Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                                duration: Duration(milliseconds: 500),
+                                content: Text("$name has been saved")));
+                        snackBar.closed
+                            .then((value) => {Navigator.pop(context)});
+                      }
+                    },
+                    child: Text("Create Contact"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void getFile() async {
+    var pickedFile = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _file = pickedFile;
+    });
   }
 }
--- phone_book/lib/contacts/ContactsPage.dart
+++ phone_book/lib/contacts/ContactsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:phone_book/contacts/ContactCreate.dart';
+import 'package:phone_book/database/db_helper.dart';
 import 'package:phone_book/model/Contact.dart';
 
 class ContactsPage extends StatefulWidget {
@@ -10,70 +11,77 @@ class ContactsPage extends StatefulWidget {
 class _ContactsPageState extends State<ContactsPage> {
   List<Contact> contacts;
 
+  DbHelper _dbHelper;
+
   @override
   void initState() {
-    contacts = Contact.contacts;
-
+    _dbHelper = DbHelper();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    Contact.contacts.sort((a, b) => b.name[0].compareTo(a.name[0]));
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Phone Book"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => ContactCreate()));
-        },
-        child: Icon(Icons.add),
-      ),
-      body: ListView.builder(
-          itemCount: contacts.length,
-          itemBuilder: (BuildContext context, int index) {
-            Contact contact = contacts[index];
-            return Container(
-              padding: EdgeInsets.all(5),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          "https://placekitten.com/200/200",
+        appBar: AppBar(
+          title: Text("Phone Book"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ContactCreate()));
+          },
+          child: Icon(Icons.add),
+        ),
+        body: FutureBuilder(
+            future: _dbHelper.getContacts(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              if (snapshot.data.isEmpty) {
+                return Text("Your Contact list empty");
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Contact contact = snapshot.data[index];
+                    return Dismissible(
+                      key: UniqueKey(),
+                      onDismissed: (direction) {
+                        _dbHelper.removeContact(contact.id);
+
+                        Scaffold.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("${contact.name} has been deleted"),
+                            action: SnackBarAction(
+                              label: "UNDO",
+                              onPressed: () {
+                                _dbHelper.insertContact(contact);
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage(
+                            contact.avatar.isEmpty
+                                ? "assets/img/person.jpg"
+                                : contact.avatar,
+                          ),
+                          child: Text(
+                            contact.name[0],
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
                         ),
-                        child: Text(
-                          contact.name[0],
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
-                        ),
+                        title: Text(contact.name),
+                        subtitle: Text(contact.phoneNumber),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(contact.name),
-                            Text(contact.phoneNumber),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Divider(
-                      height: 2,
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }),
-    );
+                    );
+                  });
+            }));
   }
 }
